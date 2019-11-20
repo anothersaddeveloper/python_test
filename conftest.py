8b579efec2160d3d46c0cbcd5c22f34949c9a9ae
Rafal Rosa <rrosa@poppulo.com>
2019-11-20 21:29:21 +0000

    Prints tests running

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ import pytest
 
 
 def pytest_addoption(parser):
+    print("Running tests")
     parser.addoption(
         "--cmdopt", action="store", default="type1", help="my option: type1 or type2"
     )
